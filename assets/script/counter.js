45fb3ea35b9ccce420e2d53355be67b12c4f15eb
AleDeb93 <121309726+AleDeb93@users.noreply.github.com>
2023-02-21 15:52:23 +0100

    Update counter.js

--- assets/script/counter.js
+++ assets/script/counter.js
@@ -1,59 +1,66 @@
-let counter = 0;
-
-let applicazione = document.createElement("div");
-applicazione.className = "applicazione";
-document.body.appendChild(applicazione);
-
-let contatore = document.createElement("div");
-contatore.innerHTML = counter;
-contatore.className = "contatore";
-applicazione.appendChild(contatore);
-
-let bottoni = document.createElement("div");
-bottoni.className = "bottoni";
-applicazione.appendChild(bottoni);
-
-let piu = document.createElement("button");
-piu.innerHTML = "+";
-piu.setAttribute("data-action", "piu");
-bottoni.appendChild(piu);;
-
-let reset = document.createElement("button");
-reset.innerHTML = "Reset";
-reset.setAttribute("data-action", "reset");
-bottoni.appendChild(reset);
-
-let meno = document.createElement("button");
-meno.innerHTML = "-";
-meno.setAttribute("data-action", "meno");
-bottoni.appendChild(meno);
-
-applicazione.addEventListener("click", function(event) {
-  if (event.target.tagName === "BUTTON") {
-    let action = event.target.getAttribute("data-action");
-    switch (action) {
-      case "piu":
-        counter++;
-        break;
-      case "meno":
-        counter--;
-        break;
-      case "reset":
-        counter = 0;
-        break;
+function createElement(tagName, classNames, innerHTML, dataAction) {
+    const element = document.createElement(tagName);
+    if (classNames) {
+      element.className = classNames;
     }
-    contatore.innerHTML = counter;
-    
-    if(counter === 20 || counter === -20){
-    alert("Ci stai prendendo gusto?");
-    } else if(counter === 40 || counter === -40){
-    alert("Fermati! Dai tregua al tuo povero mouse!");
+    if (innerHTML) {
+      element.innerHTML = innerHTML;
     }
-    
+    if (dataAction) {
+      element.setAttribute("data-action", dataAction);
+    }
+    return element;
   }
-}); 
+  
+  let counter = 0;
+  
+  const applicazione = createElement("div", "applicazione");
+  document.body.appendChild(applicazione);
+  
+  const contatore = createElement("div", "contatore", counter);
+  applicazione.appendChild(contatore);
 
-let istruzioni = document.createElement("div");
-istruzioni.className = "istruzioni";
-istruzioni.innerHTML = "Premi i pulsanti più e meno per aumentare o diminuire il valore del contatore <br> Premi il pulsante reset per far tornare il contatore a zero";
-document.body.appendChild(istruzioni);
+  const bottoni = createElement("div", "bottoni");
+  applicazione.appendChild(bottoni);
+  
+  const piu = createElement("button", null, "+", "piu");
+  const reset = createElement("button", null, "Reset", "reset");
+  const meno = createElement("button", null, "-", "meno");
+  /*
+  Predispongo un array per l'inserimento dei bottoni nel div perchè, nel caso in futuro
+  dovessi aggiungere qualcosa mi basterebbe concatenare il nuovo array al vecchio e con
+  il ciclo for questi sarebbero aggiunti automaticamente in visualizzazione nel browser
+  */
+ let bottoniArray = [piu, reset, meno];
+ for (let i = 0; i < bottoniArray.length; i++) {
+    bottoni.appendChild(bottoniArray[i]);
+ }
+  
+  applicazione.addEventListener("click", function(event) {
+    if (event.target.tagName === "BUTTON") {
+      let action = event.target.getAttribute("data-action");
+      switch (action) {
+        case "piu":
+          counter++;
+          break;
+        case "meno":
+          counter--;
+          break;
+        case "reset":
+          counter = 0;
+          break;
+      }
+      contatore.innerHTML = counter;
+
+      if(counter === 20 || counter === -20){
+        alert("Ci stai prendendo gusto non è vero??");
+      }
+      if(counter === 40 || counter === -40){
+        alert("Fermati!! Stai distruggendo il tuo povero mouse!");
+      }
+    }
+  });
+  contatore.innerHTML = counter;
+  
+  const istruzioni = createElement("div", "istruzioni", "Premi i pulsanti piu e meno per aumentare o diminuire il valore del contatore <br> Premi il pulsante Reset per far tornare il contatore a 0");
+  document.body.appendChild(istruzioni);
